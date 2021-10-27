cfe630122dd902aea154558faa846965d1ab08ec
utkarsha-kavathekar <90891816+utkarsha-kavathekar@users.noreply.github.com>
2021-10-27 16:42:06 +0530

    Demo of basic string functions in python

---
+++ string_demo.py
@@ -0,0 +1,10 @@
+##Using some basic functions to work with string
+str1="Hello Python "
+print(str1)
+print("Length of str1 is %d "%len(str1))
+print("Slicing of string : %s"%str1[6:])
+
+print(str1.join(['1','2','3']))
+print(str1.count('l'))
+
+print(str1*2)
