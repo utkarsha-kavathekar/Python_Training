9683b4f0616ad726b53e297341d52a362f6dbc2e
Utkarsha Kavathekar <utkarsha_kavathekar@persistent.com>
2021-12-01 19:19:27 +0530

    demo of Sending form data to template

---
+++ REST_Application/form_temp.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask.templating import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def student():
+    return render_template('student.html')
+
+@app.route('/result',methods=['POST','GET'])
+def result():
+    if request.method == 'POST':
+        result=request.form
+        return render_template('result.html',result=result)
+
+if __name__=='__main__':
+    app.run(debug=True)
